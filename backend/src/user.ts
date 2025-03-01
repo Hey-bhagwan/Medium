0a34c55595323c534b892c2bd142f904477eb7fe
Hey-bhagwan <ayushupa29@gmail.com>
2025-03-01 23:31:28 +0530

    fixed signup

--- backend/src/user.ts
+++ backend/src/user.ts
@@ -38,8 +38,10 @@ userRouter.post('/signup',async (c) => {
   
       const token = await sign({ id: user.id },c.env.JWT_SECRET);
     
+      
+
       return c.json({
-        jwt: token
+        token
       })
     }catch(e){
       c.status(400);
--- frontend/src/pages/auth.tsx
+++ frontend/src/pages/auth.tsx
@@ -19,7 +19,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" } ) => {
     async function sendRequest() {
         try{
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup"? "signup" : "signin"}`, postInput);
-            const jwt = response.data;
+            const jwt = type === "signup" ? response.data.token : response.data;
             localStorage.setItem("token", jwt);
             navigate("/blogs");
         }catch(e){
